8de7a74c2db4277e62ece15dce0b3cd427b5f64d
YeinKim <89799183+rladp127@users.noreply.github.com>
2022-10-05 04:23:04 +0900

    [Silver IV] Title: 괄호, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+using namespace std;
+// ())(()
+int main() {
+        int T; // test case 입력
+        cin >> T;
+        string result[T]; // 결과 저장할 배열
+
+        for (int i = 0; i < T; i++) {
+                int top = 0;
+          
+                string input; // 괄호 입력 받기
+                cin >> input;
+                int len = input.length();
+
+                string arr[len]; // 저장할 스택(괄호 길이만큼)
+
+                for (int j = 0; j < len; j++) {
+                        if (input[j] == '(') {
+                                  arr[top++] = "(";
+                          }
+                        else if(input[j] == ')') {
+                          if (top == 0) 
+                            result[i] = "NO";
+                          else top--;
+                        }
+                }
+          
+                if(top == 0 && result[i] != "NO") result[i] = "YES";
+          else if (top != 0) result[i] = "NO";
+
+        }
+
+        for (int i = 0; i < T; i++) {
+                cout << result[i] << "\n";
+        }
+
+        return 0;
+}
