4a1af18b7162cd7153d3f77f05ca79c894207169
YeinKim <89799183+rladp127@users.noreply.github.com>
2023-04-03 20:42:00 +0900

    [Unrated] Title: 문자와 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Unrated/27866. 문자와 문자열/문자와 문자열.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  string input;
+  cin >> input;
+
+  int idx;
+  cin >> idx;
+  cout << input[idx - 1];
+
+  return 0;
+}
