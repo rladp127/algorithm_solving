80e47227fda193ac16449f7cfb8fea1b3bf7a4c9
YeinKim <89799183+rladp127@users.noreply.github.com>
2023-05-18 00:03:20 +0900

    [unrated] Title: 조건에 맞는 사용자 정보 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
@@ -0,0 +1,17 @@
+# 게시물을 3개 이상 등록한 사람의 정보..
+# 두 테이블은 WRITER_ID, USER_ID
+
+# 전체주소는 시, 도로명, 상세주소
+# REGEXP_REPLACE('01012345678', '(02|.{3})(.+)(.{4})', '\\1-\\2-\\3');
+# => USER_ID, NICKNAME, 전체주소, 전화번호 (형식)
+
+# 회원 아이디 기준 DESC
+
+SELECT j.USER_ID, j.NICKNAME, 
+CONCAT(CITY, ' ', STREET_ADDRESS1, ' ', STREET_ADDRESS2) AS 전체주소, 
+CONCAT_WS("-", SUBSTRING(j.TLNO, 1, 3), SUBSTR(j.TLNO, 4, 4), RIGHT(j.TLNO, 4)) AS 전화번호
+FROM USED_GOODS_BOARD i 
+JOIN USED_GOODS_USER j ON i.WRITER_ID = j.USER_ID
+GROUP BY i.WRITER_ID
+HAVING COUNT(*) > 2
+ORDER BY i.WRITER_ID DESC;
