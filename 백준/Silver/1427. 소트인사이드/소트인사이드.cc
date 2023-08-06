9f7d6020fae7060b2828b19579058a347e462144
YeinKim <89799183+rladp127@users.noreply.github.com>
2023-08-06 15:16:59 +0900

    [Silver V] Title: 소트인사이드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.cc
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.cc
@@ -1,50 +1,25 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
-string arr;
-string input;
-
-void merge (int s, int m, int e){
-    int i = s; 
-    int k = s; // Sorting 시작되는 첫 index
-    int j = m+1; // 가운데+1번째 index
-
-    while (i <= m && j <= e) { // 비교하면서 원소 넣기
-        if (input[i] >= input[j])
-            arr[k++] = input[i++];
-        else arr[k++] = input[j++];
-    }
-
-    if (i > m) { // 원소가 남으면 그대로 배열에 넣음 (이미 정렬되어 있으므로)
-        for (int z = j; z <= e; z++) 
-            arr[k++] = input[z];
-    } else {
-        for (int z = i; z <= m; z++) 
-            arr[k++] = input[z];
-    }
-
-    for(int z = s; z <= e; z++) {
-        input[z] = arr[z];
-    }
-    
-}
-
-void mergesort(int start, int end) {
-    if (start < end) {
-        int mid = (start+end)/2;
-        mergesort(start, mid);
-        mergesort(mid+1, end);
-        merge(start, mid, end);
-    }
-}
+vector<int> v;
 
 int main() {
-    cin >> input;
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+	int N;
+	cin >> N;
 
-    mergesort(0,input.length()-1);
-
-    for (int i = 0; i < input.length(); i++) {
-        cout << input[i];
+    while (N > 0) {
+        v.push_back(N%10);
+        N /= 10;
     }
 
-    return 0;
+    sort(v.begin(), v.end());
+
+    for (int i = v.size()-1; i >= 0; i--) {
+        cout << v[i];
+    }
+
+	return 0;
 }
