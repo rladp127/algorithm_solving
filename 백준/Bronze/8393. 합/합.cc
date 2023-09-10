5738bbacea67bd5c87233e9d152357245824a846
YeinKim <89799183+rladp127@users.noreply.github.com>
2023-09-11 08:55:49 +0900

    [Bronze V] Title: 합, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num; 
+    cin >> num;
+
+    int sum = 0;
+    for (int i = 1; i <= num; i++) {
+        sum += i;
+    }
+
+    cout << sum;
+    
+    return 0;
+}
