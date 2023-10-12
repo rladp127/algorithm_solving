ef0f6b5db4f7e5fb9a219c5ebb156ff8cfd8b0ef
YeinKim <89799183+rladp127@users.noreply.github.com>
2023-10-12 18:12:35 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cin.tie(0); 
+    cout.tie(0); 
+    ios::sync_with_stdio(false);
+
+    cout << "         ,r'\"7\n" 
+         << "r`-_   ,'  ,/\n" 
+         << " \\. \". L_r'\n" 
+         << "   `~\\/\n" 
+         << "      |\n" 
+         << "      |" << '\n';
+
+    return 0;
+}
+
+/*
+         ,r'"7
+r`-_   ,'  ,/
+ \. ". L_r'
+   `~\/
+      |
+      |
+*/
